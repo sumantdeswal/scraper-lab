7d5e39061e5ea239111727fbd735e22438ca69cb
Sumant Deswal <sumantd1206@gmail.com>
2026-06-24 17:53:54 +0530

    Update

--- app.py
+++ app.py
@@ -3,9 +3,32 @@ import os
 
 app = Flask(__name__)
 
+CHALLENGES = {
+    1: {
+        "name": "Plain HTML Images",
+        "difficulty": "Easy"
+    }
+}
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template(
+        "home.html",
+        challenges=CHALLENGES
+    )
+
+@app.route("/challenge/<int:challenge_id>")
+def challenge(challenge_id):
+
+    challenge = CHALLENGES.get(challenge_id)
+
+    if not challenge:
+        return "Challenge Not Found", 404
+
+    return render_template(
+        f"challenge_{challenge_id}.html",
+        challenge=challenge
+    )
 
 if __name__ == "__main__":
     app.run(
