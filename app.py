614d2444bb89e8bd6a709d79a5941749575a4897
Sumant Deswal <sumantd1206@gmail.com>
2026-06-25 10:07:30 +0530

    Update

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+from data.challenges import CHALLENGES
 
-CHALLENGES = {
-    1: {
-        "name": "Plain HTML Images",
-        "difficulty": "Easy"
-    }
-}
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -17,16 +12,15 @@ def home():
         challenges=CHALLENGES
     )
 
-@app.route("/challenge/<int:challenge_id>")
+@app.route("/challenge/<challenge_id>")
 def challenge(challenge_id):
-
     challenge = CHALLENGES.get(challenge_id)
 
     if not challenge:
         return "Challenge Not Found", 404
 
     return render_template(
-        f"challenge_{challenge_id}.html",
+        "challenge.html",
         challenge=challenge
     )
 
---
+++ data/challenges.py
@@ -0,0 +1,8 @@
+CHALLENGES = {
+    "html-images": {
+        "number": 1,
+        "title": "HTML Images",
+        "description": "Standard HTML image elements.",
+        "template": "challenges/html-images.html"
+    }
+}
