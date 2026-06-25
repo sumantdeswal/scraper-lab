ada8b75103fcbaf119c18d5ee45be3a20b8bb088
Sumant Deswal <sumantd1206@gmail.com>
2026-06-25 10:13:54 +0530

    Update

--- data/challenges.py
+++ data/challenges.py
@@ -4,5 +4,29 @@ CHALLENGES = {
         "title": "HTML Images",
         "description": "Standard HTML image elements.",
         "template": "challenges/html-images.html"
+    },
+    "srcset-images": {
+        "number": 2,
+        "title": "Srcset Images",
+        "description": "Responsive image delivery using srcset and sizes.",
+        "template": "challenges/srcset-images.html"
+    },
+    "picture-element": {
+        "number": 3,
+        "title": "Picture Element",
+        "description": "Adaptive delivery using the picture element.",
+        "template": "challenges/picture-element.html"
+    },
+    "css-background-images": {
+        "number": 4,
+        "title": "CSS Background Images",
+        "description": "Images delivered through CSS background properties.",
+        "template": "challenges/css-background-images.html"
+    },
+    "lazy-loading": {
+        "number": 5,
+        "title": "Lazy Loading",
+        "description": "Images that load only when they are needed.",
+        "template": "challenges/lazy-loading.html"
     }
 }
